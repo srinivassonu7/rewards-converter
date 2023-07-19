e1a50af5b2dad05680de77fe2cc35623a3413685
srinivassonu7 <41520796+srinivassonu7@users.noreply.github.com>
2023-07-20 03:50:24 +0530

    Add files via upload
    modified

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class RewardValue{
+    private double cValue;
+    private final double rate=0.0035;
+    RewardValue(double cValue){
+        this.cValue=cValue;
+    }
+    RewardValue(int mValue){
+        this.cValue=convertToCash(mValue);
+    }
+    double convertToCash(int mValue){
+        return mValue * rate;
+    }
+    double getCashValue(){
+        return cValue;
+    }
+    int getMilesValue(){
+        return (int)(this.cValue/rate);
+    }
+
+}
